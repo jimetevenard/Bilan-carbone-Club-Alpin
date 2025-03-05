0c57926dbbcbc35fbe1c58c9cf7afb85fcb5219d
Jim ETEVENARD <jimetevenard@gmail.com>
2025-03-05 22:42:29 +0100

    Ajout de types supplémentaires
    Ajout des types qui ne figuraient pas dans le fichier test

--- logique/types-transport.js
+++ logique/types-transport.js
@@ -3,7 +3,9 @@
  * (Ici, normalisés en capitales, car il y a quelques variations dans le fichier)
  */
 const TYPES_CAF = {
+   AUTOBUS: 'AUTOBUS', 
    AUTOCAR: 'AUTOCAR',
+   AUTOCAR_COUCHETTE: 'AUTOCAR-CAR-COUCHETTE',
    AVION: 'AVION',
    BATEAU_FERRY: 'BATEAU-FERRY',
    BATEAU_VEDETTE: 'BATEAU-VEDETTE',
@@ -18,6 +20,8 @@ const TYPES_CAF = {
    TRAIN_TER: 'TRAIN-TER',
    TRAIN_TRANSILIEN: 'TRAIN-TRANSILIEN',
    TRAIN_TGV: 'TRAIN-TGV',
+   TRAIN_NUIT: 'TRAIN-DE-NUIT',
+   TRAMWAY: 'TRAMWAY',
 };
 
 /**
@@ -91,11 +95,11 @@ function checkTypeExiste(typeCaf) {
 function mappingVersTypesGoogle(typeCaf) {
     checkTypeExiste(typeCaf);
 
-    if(TYPES_CAF.METRO === typeCaf || typeCaf.startsWith('TRAIN')){
+    if([TYPES_CAF.METRO, TYPES_CAF.TRAMWAY].includes(typeCaf) || typeCaf.startsWith('TRAIN')){
         return [TYPES_GOOGLE.TRANSIT];
     }
 
-    if(TYPES_CAF.BUS === typeCaf){
+    if([TYPES_CAF.BUS, TYPES_CAF.AUTOBUS].includes(typeCaf)){
         /**
          * NOTE : Google Maps ne trouve pas toujours les liaisons en bus
          * Ex. cas du trajet 2 de la sortie 24-RW15	"Madère, la perle de l'Atlantique"
@@ -145,18 +149,19 @@ function mappingVersTypesADEME(typeCaf) {
             throw new Error('Transports maritimes non pris en charge')
 
         case TYPES_CAF.BUS:
-            return TYPES_ADEME.BUS_THERMIQUE // Hypothèse - Les bus en circulation en France sont majoritairement thermiques, surtout en région.
+        case TYPES_CAF.AUTOBUS:
+            return TYPES_ADEME.BUS_THERMIQUE // Les bus en circulation en France sont majoritairement thermiques, surtout en région.
 
         case TYPES_CAF.COVOITURAGE:
-            return TYPES_ADEME.COVOITURAGE_THERMIQUE_3_PASSAGERS // Hypothèse à valider
+            return TYPES_ADEME.COVOITURAGE_THERMIQUE_3_PASSAGERS
 
         case TYPES_CAF.METRO:
             return TYPES_ADEME.METRO
 
         case TYPES_CAF.MINIBUS:
-            return TYPES_ADEME.COVOITURAGE_THERMIQUE_4_PASSAGERS // Hypothèse à valider
+            return TYPES_ADEME.COVOITURAGE_THERMIQUE_4_PASSAGERS
         case TYPES_CAF.TAXI_VOITURE:
-            return TYPES_ADEME.COVOITURAGE_THERMIQUE_4_PASSAGERS// Hypothèse à valider
+            return TYPES_ADEME.COVOITURAGE_THERMIQUE_4_PASSAGERS
 
         case TYPES_CAF.TRAIN_INTERCITES:
             return TYPES_ADEME.INTERCITES
@@ -167,7 +172,39 @@ function mappingVersTypesADEME(typeCaf) {
             return TYPES_ADEME.TER
         case TYPES_CAF.TRAIN_TGV:
             return TYPES_ADEME.TGV
+        case TYPES_CAF.TRAMWAY:
+            return TYPES_ADEME.TRAMWAY
+
+
+        // Cas particuliers - Pourront être amenés à être révisés par la comission
+        // =======================================================================
+
+        case TYPES_CAF.AUTOCAR_COUCHETTE:
+            /**
+             * L'immatriculation de car-couchettes est interdite en France depuis 2011.
+             * L'âge moyen du parc roulant de car-couchettes est donc nettement plus
+             * elvé que la moyenne des autocars. Faute de prise en charge de ca cas
+             * par l'ADEME, on le considère ici comme un car classique
+             */
+            return TYPES_ADEME.AUTOCAR_THERMIQUE
+        
+        case TYPES_CAF.TRAIN_NUIT:
+            /**
+             * L'ADEME de prend pas en charge les trains de nuit.
+             * 
+             * Des études existent, voir par exemple celle d'Objectif Carbone :
+             * http://www.objectifcarbone.org/wp-content/uploads/2024/03/Objectif_Carbone_Note_TDN.pdf
+             * 
+             * A noter que nos sorties empreintent majoritairement la ligne Paris-Briançon,
+             * qui n'est pas electrifié sur une partie significative de son parcours.
+             * 
+             * Pour l'instant, on l'affecte en intercité.
+             */
+            return TYPES_ADEME.INTERCITES
     }
+
+    // Erreur si l'on rencontre un type non mappé ci-dessus
+    throw new Error(`Pas d'équivalence ADEME pour le type ${typeCaf}`);
 }
 
 /**
@@ -189,10 +226,12 @@ function mappingVersTypeDeCalcul(typeCaf) {
  * 
  * - Le fichier d'origine contentien parfois des minuscules, parfois des minuscules.
  *   On passe tout en majuscules.
- * - S'il contient des espaces, on ne garde que ce qui est avant. (d'où le split)
+ * - S'il contient des espaces et/ou des tirets, on remplace par un tiret
+ *   Plusieurs espaces et/ou tirets à la suite seront remplacés par un seul tiret
  */
 function normaliser(typeCafBrut) {
-    return typeCafBrut.toUpperCase().split(' ')[0];
+    const REGEX_ESPACES_TIRETS = /[ _-]+/g;
+    return typeCafBrut.toUpperCase().replaceAll(REGEX_ESPACES_TIRETS,'-');
 }
 
 module.exports = {
